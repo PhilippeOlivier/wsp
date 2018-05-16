f1aa14314c16344dbec17e2d717684d5e5791942
Philippe Olivier <philippe.olivier@polymtl.ca>
2018-05-16 10:42:40 -0400

    Removed bap and gap.

--- models/ipb/main.cc
+++ models/ipb/main.cc
@@ -12,10 +12,7 @@ void Help();
 
 
 int main(int argc, char* argv[]) {
-
-    bool bap = false;
     char* filename;
-    IloNum gap = 0.01;
     IloInt norm = 1;
     IloNum time_limit = IloNumMax;
 
@@ -28,20 +25,13 @@ int main(int argc, char* argv[]) {
 	exit(0);
     }
     for (int i=1; i<argc; i++) {
-	if (!strcmp(argv[i], "-bap")) {
-	    bap = true;
-	}
-	else if (!strcmp(argv[i], "-bins")) {
+	if (!strcmp(argv[i], "-bins")) {
 	    i++;
 	    num_bins = atoi(argv[i]);
 	}
 	else if (!strcmp(argv[i], "-file")) {
 	    i++;
 	    filename = argv[i];
-	}
-	else if (!strcmp(argv[i], "-gap")) {
-	    i++;
-	    gap = atof(argv[i]);
 	}
 	else if (!strcmp(argv[i], "-maxdev")) {
 	    i++;
@@ -65,7 +55,6 @@ int main(int argc, char* argv[]) {
 	(num_bins <= 0) ||
 	(min_deviation < 0) ||
 	(max_deviation < min_deviation) ||
-	(gap < 0) ||
 	(norm != 1 && norm != 2) ||
 	(time_limit < 0)) {
 	Help();
@@ -91,14 +80,9 @@ void Help() {
 	 << endl;
     cout << "./wsp [options]"
 	 << endl;
-    cout << "-bap (use the branch and price algorithm; "
-	 << "default: best integer solution with initial RMP columns)]"
-	 << endl;
     cout << "-bins [number of bins]"
 	 << endl;
     cout << "-file [filename.wpn]"
-	 << endl;
-    cout << "-gap [relative gap (default 1%, or 0.01)]"
 	 << endl;
     cout << "-mindev [minimum cumulative deviation (default unbounded)]"
 	 << endl;
